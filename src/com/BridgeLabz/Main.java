52d7696a9896e482ab2af71bcc4e61d731fd7325
pihu2810 <vishaka.kadam01@gmail.com>
2022-04-08 14:46:45 +0530

    add OrderedList

--- src/com/BridgeLabz/Main.java
+++ src/com/BridgeLabz/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 	UnOrderedList<String> unOrderedList=new UnOrderedList<>();
+	System.out.println("UnOrderedList");
 	unOrderedList.add("189");
 	unOrderedList.add("Number");
 	unOrderedList.add("Batch");
@@ -20,7 +21,23 @@ public class Main {
 	unOrderedList.pop();
 	unOrderedList.pop(2);
 	unOrderedList.printList();
-
+	
+	OrderedList<Integer> orderedList=new OrderedList<>();
+	System.out.println("");
+	System.out.println("OrderedList");
+	orderedList.add(57);
+	orderedList.add(89);
+	orderedList.add(23);
+	orderedList.add(97);
+	orderedList.add(34);
+	orderedList.add(12);
+	orderedList.remove(57);
+	orderedList.search(34);
+	orderedList.size();
+	orderedList.index(89);
+	orderedList.pop();
+	orderedList.pop(1);
+	orderedList.printList();
 	}
 
 }
